44c5196f401021329831203f20ea43dd927a18cb
Miranda Zheng <zhengshuang2011@gmail.com>
2021-11-01 20:36:08 +0000

    Add isEven function to check whether number is even or not

--- hello.js
+++ hello.js
@@ -5,3 +5,9 @@ const sayHello = function (name) {
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
+
+const returnSayHello = function (name) {
+  return "Hello, " + name;
+};
+const greeting = returnSayHello("John");
+console.log(greeting);
