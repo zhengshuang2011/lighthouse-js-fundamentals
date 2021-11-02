898373db19cb4788eed42a717fcc08ca1cb53ce7
Miranda Zheng <zhengshuang2011@gmail.com>
2021-11-02 03:56:07 +0000

    Created a packingList to index every item by using for and while

--- packing-list.js
+++ packing-list.js
@@ -15,3 +15,10 @@ for (let i = 0; i < packingList.length; i++) {
 }
 
 console.log(packingList[3]);
+
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
